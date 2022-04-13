7f6ba84bbdab07e01a4a631aba21fb6c8b600861
kkc23 <katherine.chu@kellogg.northwestern.edu>
2022-04-13 04:58:27 +0000

    finished rest of hw

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,7 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
+select listings.neighborhood, count(reviews.id)
+from listings inner join reviews on reviews.listing_id = listings.id
+group by listings.neighborhood;
 
--- practice-7.sql
+++ practice-7.sql
@@ -16,4 +16,7 @@
 -- | Entire townhouse                    | 3588     | 2021-10-17                 |
 -- | Entire villa                        | 75       | 2021-10-12                 |
 
+select listings.property_type, count(reviews.id), max(reviews.date_reviewed)
+from listings inner join reviews on reviews.listing_id = listings.id
+group by listings.property_type;
 
